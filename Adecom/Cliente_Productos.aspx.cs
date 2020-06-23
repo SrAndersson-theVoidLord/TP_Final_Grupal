0371615d53251dea898a0f669c62bcfd9d3de398
shionx9 <55811800+shionx9@users.noreply.github.com>
2020-06-23 20:25:03 -0300

    Revert "Merge branch 'master' of https://github.com/SrAndersson-theVoidLord/TP_Final_Grupal"
    This reverts commit 9e895714b9728a07b84b888797db51609956a1dd, reversing
    changes made to 02414f3d9b294e4c1d185d82119447a2eec91fa9.

--- Adecom/Cliente_Productos.aspx.cs
+++ Adecom/Cliente_Productos.aspx.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace Adecom
 {
@@ -18,95 +16,6 @@ namespace Adecom
 
         protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
-        }
-
-        protected void btn_añadir_producto(object sender, CommandEventArgs e)
-        {
-
-            if (e.CommandName == "Comando_Añadir")
-            {
-
-                if (Session["Carrito"] == null)
-                {
-
-                    Session["Carrito"] = Crear_tabla();
-
-                }
-
-                string[] datos = new string[6];
-                datos = e.CommandArgument.ToString().Split(';');
-
-                bool flag = false;
-                DataTable dt = (DataTable)Session["Carrito"];
-                for (int i = 0; i < dt.Rows.Count; i++)
-                {
-                    if (datos[0] == dt.Rows[i]["ID"].ToString())
-                    {
-                        flag = true;
-                    }
-                }
-
-                if (!flag)
-                {
-                    int ID_Producto = Convert.ToInt32(datos[0]);
-                    string Categoria_Producto = datos[1];
-                    string Nombre_Producto = datos[2];
-                    string Descripcion_Producto = datos[3];
-                    string Imagen_Producto = datos[4];
-                    float Precio_Producto = Convert.ToSingle(datos[5]);
-
-                    Crear_columna((DataTable)Session["Carrito"], ID_Producto, Categoria_Producto, Nombre_Producto, Descripcion_Producto, Imagen_Producto, Precio_Producto);
-                }
-            }
-
-        }
-
-        public DataTable Crear_tabla()
-        {
-
-            DataTable aux_table = new DataTable();
-            DataColumn aux_column = new DataColumn();
-
-            aux_column = new DataColumn("ID", System.Type.GetType("System.Int32"));
-            aux_table.Columns.Add(aux_column);
-
-            aux_column = new DataColumn("Categoria", System.Type.GetType("System.String"));
-            aux_table.Columns.Add(aux_column);
-
-            aux_column = new DataColumn("Nombre", System.Type.GetType("System.String"));
-            aux_table.Columns.Add(aux_column);
-
-            aux_column = new DataColumn("Descripcion", System.Type.GetType("System.String"));
-            aux_table.Columns.Add(aux_column);
-
-            aux_column = new DataColumn("Imagen", System.Type.GetType("System.String"));
-            aux_table.Columns.Add(aux_column);
-
-            aux_column = new DataColumn("Precio", System.Type.GetType("System.Single"));
-            aux_table.Columns.Add(aux_column);
-
-            aux_column = new DataColumn("Cantidad", System.Type.GetType("System.Int32"));
-            aux_table.Columns.Add(aux_column);
-
-            return aux_table;
-
-        }
-
-        public void Crear_columna(DataTable aux_table, int ID_Producto, string Categoria_Producto, string Nombre_Producto, string Descripcion_Producto, string Imagen_Producto, float Precio_Producto)
-        {
-
-            DataRow aux_columna = aux_table.NewRow();
-
-            aux_columna["ID"] = ID_Producto;
-            aux_columna["Categoria"] = Categoria_Producto;
-            aux_columna["Nombre"] = Nombre_Producto;
-            aux_columna["Descripcion"] = Descripcion_Producto;
-            aux_columna["Imagen"] = Imagen_Producto;
-            aux_columna["Precio"] = Precio_Producto;
-            aux_columna["Cantidad"] = 1;
-
-            aux_table.Rows.Add(aux_columna);
 
         }
     }
